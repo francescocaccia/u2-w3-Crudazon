05a6a8c8dbbf9270d746e14157ae454572cb121e
francescocaccia <123968277+francescocaccia@users.noreply.github.com>
2023-03-17 16:53:17 +0100

    test

--- assets/script/back.js
+++ assets/script/back.js
@@ -1,5 +1,5 @@
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MmUxMmY4MWI0MjAwMTM5YjI3ZGYiLCJpYXQiOjE2NzkwNDQxMTUsImV4cCI6MTY4MDI1MzcxNX0.GpFrU8_YYPPGoAlQVOJxnVlLRSmHjTLzVaccy2Sfyec";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MmUxMmY4MWI0MjAwMTM5YjI3ZGYiLCJpYXQiOjE2NzkwNjA2MDEsImV4cCI6MTY4MDI3MDIwMX0.0N4lvm_9ze56ENmkWVaHRrC-5F0JvIKIRlsuEFNFuys";
 
 const Url = "https://striveschool-api.herokuapp.com/api/product/";
 const URLParams = new URLSearchParams(window.location.search);
@@ -7,15 +7,6 @@ const selectedId = URLParams.get("id");
 
 const endpoint = selectedId ? Url + selectedId : Url;
 const method = selectedId ? "PUT" : "POST";
-
-fetch(endpoint, {
-  method,
-  body: JSON.stringify(newProduct),
-  headers: {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  },
-});
 
 window.onload = () => {
   if (selectedId) {
@@ -31,11 +22,11 @@ window.onload = () => {
     })
       .then(res => res.json())
       .then(data => {
-        const { name, description, brand, imgUrl, price } = data;
+        const { name, description, brand, imageUrl, price } = data;
         document.getElementById("name").value = name;
         document.getElementById("description").value = description;
         document.getElementById("brand").value = brand;
-        document.getElementById("imgUrl").value = imgUrl;
+        document.getElementById("imgUrl").value = imageUrl;
         document.getElementById("price").value = price;
       })
       .catch(error => console.log(error));
@@ -43,6 +34,7 @@ window.onload = () => {
 };
 
 const gestisciSubmit = event => {
+  event.preventDefault();
   const newProduct = {
     name: document.getElementById("name").value,
     description: document.getElementById("description").value,
@@ -51,7 +43,7 @@ const gestisciSubmit = event => {
     price: document.getElementById("price").value,
   };
   fetch(endpoint, {
-    method: method,
+    method,
     body: JSON.stringify(newProduct),
     header: {
       Authorization: `Bearer ${token}`,
@@ -61,7 +53,7 @@ const gestisciSubmit = event => {
 };
 
 const cancellaProdotto = () => {
-  const hasAccepted = confirm("Sei convinto di voler eliminare il prodotto?");
+  const hasAccepted = confirm("Sei sicuro di voler eliminare il prodotto?");
   if (hasAccepted) {
     fetch(endpoint, {
       method: "DELETE",
